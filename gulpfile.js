81dd8965cb00c277298381e7a7a49be5d5400d37
leon.shi <superRaytin@163.com>
2015-12-30 00:02:38 +0800

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+﻿
+var fs = require('fs');
+var gulp = require('gulp');
+var rename = require("gulp-rename");
+var gulpGithubStylePage = require('./lib/index');
+
+gulp.task('test', function() {
+    return gulp.src('./README.md')
+        .pipe(gulpGithubStylePage())
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('test'));
+});
+
+gulp.task('test2', function() {
+    gulp.src('./README.md')
+        .pipe(gulpGithubStylePage({
+            template: 'project',
+            vars: {
+                pkg: {
+                    name: 'awesomeProject',
+                    version: '0.1.0'
+                },
+                examples: ['simple']
+            }
+        }))
+        .pipe(rename('index2.html'))
+        .pipe(gulp.dest('build'));
+});
